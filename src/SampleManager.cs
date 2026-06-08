eb8dd9b832279e6bdb3c27be7e6178bf6b3d4886
Fatima Elwan <167696864+Fatima-elwan@users.noreply.github.com>
2026-06-08 21:54:26 +0300

    Upload initial source code version

---
+++ src/SampleManager.cs
@@ -0,0 +1,21 @@
+﻿namespace SampleTrackingModule;
+public class SampleManager
+{
+    public string MoveToNextStage(bool isDoc, bool isStored, bool isDamaged, string currentStage)
+    {
+        if (isDamaged) return "Error: Sample is damaged.";
+        if (!isStored) return "Error: Sample not stored.";
+        if (!isDoc) return "Error: Previous stage not documented.";
+
+        if (currentStage == "Registration")
+            return "Success: Stage documented. Moving to Lab Testing.";
+        
+        else if (currentStage == "LabTesting")
+            return "Success: Stage documented. Moving to Review.";
+        
+        else if (currentStage == "Review")
+            return "Success: Stage documented. Moving to Storage.";
+        
+        return "Error: Unknown stage provided."; 
+    }
+}
